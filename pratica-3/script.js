cfb52c17510fa59b9a1e17c096919e11da107f09
João Pedro Lima <joaopedroca90@gmail.com>
2025-03-15 21:04:08 -0300

    feat: insiro lógica de adicionar e remover tasks

---
+++ pratica-3/script.js
@@ -0,0 +1,22 @@
+const addTask = function () {
+  const taskName = document.getElementById("task-input").value;
+  if (taskName === "") {
+    alert("Insira um nome de tarefa válido!");
+    return;
+  }
+
+  const taskList = document.getElementById("task-list");
+
+  const item = document.createElement("li");
+  item.classList.add("task");
+  item.innerHTML = `
+    <span id="span-task" onclick="toggleTask(this)">${taskName}</span>
+    <button id="btn-delete-task" onclick="deleteTask(this)" type="button">Delete</button>
+  `;
+
+  taskList.appendChild(item);
+};
+
+const deleteTask = function(btn) {
+    btn.parentElement.remove();
+}
