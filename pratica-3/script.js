b9b3805ab71ba6d9a5ad1d33b7f608990d5ff2ad
João Pedro Lima <joaopedrocal90@gmail.com>
2025-03-17 13:24:05 -0300

    feat: finalizo o salvamento e recuperação de dados no localstorage

--- pratica-3/script.js
+++ pratica-3/script.js
@@ -1,7 +1,7 @@
 const addTask = function () {
-  const taskName = document.getElementById("task-input").value;
+  let taskName = document.getElementById("task-input").value;
   if (taskName === "") {
-    alert("Insira um nome de tarefa válido!");
+    alert("O campo de inserção do nome da tarefa está em branco!");
     return;
   }
 
@@ -10,20 +10,54 @@ const addTask = function () {
   const item = document.createElement("li");
   item.classList.add("task");
   item.innerHTML = `
-    <span id="span-task" onclick="toggleTask(this)">${taskName}</span>
-    <button id="btn-delete-task" onclick="deleteTask(this)" type="button">Delete</button>
+  <span id="span-task" onclick="toggleTask(this)">${taskName}</span>
+  <button id="btn-delete-task" onclick="deleteTask(this)" type="button">Delete</button>
   `;
 
   taskList.appendChild(item);
+  document.getElementById("task-input").value = "";
+  saveTask();
 };
 
 const deleteTask = function (btn) {
   btn.parentElement.remove();
+  saveTask();
 };
 
 const toggleTask = function (span) {
-  span.style.setProperty("text-decoration", "solid black line-through 2px");
+  span.classList.toggle("completed");
+  saveTask();
 };
 
-const saveTask = function () {};
-const loadTask = function () {};
+const saveTask = function () {
+  const tasks = [];
+  document.querySelectorAll(".task").forEach((task) => {
+    tasks.push({
+      taskName: task.querySelector("span").innerText,
+      status: task.querySelector("span").classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+const loadTask = function () {
+  const taskList = document.getElementById("task-list");
+  taskList.innerHTML = "";
+
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  tasks.forEach((task) => {
+    const item = document.createElement("li");
+    item.classList.add("task");
+    item.innerHTML = `
+    <span class="span-task ${
+      task.status ? "completed" : ""
+    }" onclick="toggleTask(this)">${task.taskName}</span>
+    <button class="btn-delete-task" onclick="deleteTask(this)" type="button">Delete</button>
+    `;
+    taskList.appendChild(item);
+  });
+};
+
+document.addEventListener("DOMContentLoaded", loadTask);
