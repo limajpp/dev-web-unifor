a07c53d4d876efea95f679ae0d7469277a64a6c1
João Pedro Lima <joaopedrocal90@gmail.com>
2025-04-06 21:02:55 -0300

    feat: layout da aplicação inicial

---
+++ av-pratica-2/script.js
@@ -0,0 +1,5 @@
+const inputValor = document.getElementById("input-valor");
+const inputDescricao = document.getElementById("input-descricao");
+const selectCategoria = document.getElementById("select-categoria");
+
+const saveFields = function () {};
