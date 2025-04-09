61d4c2cd6ac1f8644a207219d821784b2ab4628c
João Pedro Lima <joaopedrocal90@gmail.com>
2025-04-08 21:20:49 -0300

    feat: atividade 3

---
+++ atv-3/src/main.js
@@ -0,0 +1,6 @@
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
